90824b42246d71c1b329b8032850c4985c2d918a
Ayşe Elif Çelik <celik.ayse@std.izu.edu.tr>
2024-12-18 17:15:37 +0300

    cinemachine+gravity

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,37 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed = 7f;
+    [SerializeField] private CharacterController controller;
+    [SerializeField] private Transform cam;
+    [SerializeField] private float moveSpeed = 6f;
+    [SerializeField] private float turnSmoothTime = 0.1f;
+    [SerializeField] private float gravity = -9.81f; 
+    private Vector3 velocity;
+    private float turnSmoothVelocity;
 
     private bool isWalking;
 
+    private void Start() {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
     private void Update() {
-        Vector2 inputVector = new Vector2(0, 0);
+        
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
-        if(Input.GetKey(KeyCode.W)) {
-            inputVector.y += +1;
-        }
-        if(Input.GetKey(KeyCode.S)) {
-            inputVector.y += -1;
-        }
-        if(Input.GetKey(KeyCode.A)) {
-            inputVector.x += -1;
-        }
-        if(Input.GetKey(KeyCode.D)) {
-            inputVector.x += +1;
+        isWalking = direction != Vector3.zero;
+        
+        if (direction.magnitude >= 0.1f) {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * moveSpeed * Time.deltaTime);
         }
 
-        inputVector = inputVector.normalized;
+            velocity.y += gravity * Time.deltaTime;
 
-        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
-        transform.position += moveDir * moveSpeed * Time.deltaTime;
+            if (controller.isGrounded && velocity.y < 0) {
+                velocity.y = -2f;
+            }
 
-        isWalking = moveDir != Vector3.zero;
-        float rotateSpeed = 10f;
-        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
+            controller.Move(velocity * Time.deltaTime);
     }
 
     public bool IsWalking() {
